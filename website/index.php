3f5092e153a26208fadecaeb49bb92e0f1a433f2
Drilon <drkras07@gmail.com>
2021-05-25 09:28:44 +0200

    Final

---
+++ website/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link REL=StyleSheet HREF="css/stylesheet.css" TYPE="text/css" MEDIA=screen>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M133 - Everone</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
+        <a class="navbar-brand" href="#">Everone</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item text-light">
+                info@everone.me</br>
+                +41 79 666 70 70
+            </li>
+            </ul>
+            <span class="navbar-text">
+                <a class="imprint" href="../html/imprint.html">Imprint</a>
+            </span>
+        </div>
+    </nav>
+    <center>
+        <img src="/images/everone-logo.png" class="logo-1"/>
+        <p></p>
+        <a href="./php/register.php"><button type="button" class="btn btn-primary" id="register">Register</button></a>
+        <a href="./php/login.php"><button type="button" class="btn btn-primary" id="login">Login</button></a>
+    </center>
+    
+    </body>
+</html>
